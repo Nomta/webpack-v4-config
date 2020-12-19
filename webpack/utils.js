dbb77ae06c549730cbdb19716327c945495bfd24
Nomta <nomta@mail.ru>
2020-12-19 16:24:20 +0300

    ref

--- webpack/utils.js
+++ webpack/utils.js
@@ -1,5 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development';
 
+/* loaders */
+
 exports.getBabelLoader = function(...presets) {
     return {
         loader: 'babel-loader',
@@ -20,46 +22,22 @@ exports.getCSSLoaders = function(...loaders) {
     }, 'css-loader', ...loaders];
 }
 
-exports.getFileName = function(ext) {
-    const { fileNamePatterns } = require('./config');
-    const pattern = fileNamePatterns[process.env.NODE_ENV];
-
-    return `${pattern}.${ext}`;
-}
-
-
 exports.getFileLoaders = function(dirName) {
     const path = require('path');
     
     return [{
         loader: 'file-loader',
         options: {
-            name:  path.resolve(__dirname, dirName, exports.getFileName('[ext]'))
+            name:  path.resolve(__dirname, dirName, getFileName('[ext]'))
         }
     }];
 }
-exports.getPatternsToCopy = function(files, src, dist) {
-    const path = require('path');
-    
-    return files.map(filename => ({
-        from: path.join(__dirname, src, filename),
-        to: path.join(__dirname, dist)
-    }));
-}
 
-/* pathnames */
-
-const getExtensionType = function(ext) {
-    if (/js|jsx|ts/.test(ext)) return 'scripts';
-    if (/(c|sa|sc)ss/.test(ext)) return 'styles';
-    if (/png|jpg|svg|gif/.test(ext)) return 'images';
-    if (/ttf|woff2?|eot/.test(ext)) return 'fonts';
-    return 'assets';
-}
+/* paths */
 
 exports.getPathName = function(ext) {
     if (isDev) {
-        return exports.getFileName(ext);
+        return getFileName(ext);
     }
     const { dirnames, fileNamePatterns } = require('./config');
     const pattern = fileNamePatterns['production'];
@@ -74,4 +52,30 @@ exports.getPathNames = function(src) {
     const { dirnames } = require('./config');
     
     return mapValues(dirnames, dirname => path.relative(src, dirname));
+}
+
+exports.getPatternsToCopy = function(files, src, dist) {
+    const path = require('path');
+    
+    return files.map(filename => ({
+        from: path.join(__dirname, src, filename),
+        to: path.join(__dirname, dist)
+    }));
+}
+
+/* internal */
+
+function getExtensionType(ext) {
+    if (/js|jsx|ts/.test(ext)) return 'scripts';
+    if (/(c|sa|sc)ss/.test(ext)) return 'styles';
+    if (/png|jpg|svg|gif/.test(ext)) return 'images';
+    if (/ttf|woff2?|eot/.test(ext)) return 'fonts';
+    return 'assets';
+}
+
+function getFileName(ext) {
+    const { fileNamePatterns } = require('./config');
+    const pattern = fileNamePatterns[process.env.NODE_ENV];
+
+    return `${pattern}.${ext}`;
 }

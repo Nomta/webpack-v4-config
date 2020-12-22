63457848131a26d55c9abae4188c1b9928b462bd
Nomta <nomta@mail.ru>
2020-12-22 09:11:59 +0300

    Update webpack.config.plugins.js

--- webpack/webpack.config.plugins.js
+++ webpack/webpack.config.plugins.js
@@ -1,16 +1,9 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
 const webpack = require('webpack');
-
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { getPathName, getPatternsToCopy, getPathNames } = require('./utils');
 const { alias, filesToBeCopy, template } = require('./config');
-
 const pathnames = getPathNames(__dirname);
-
 const isDev = process.env.NODE_ENV === 'development';
 
 /* plugins */
@@ -22,14 +15,17 @@ const plugins = [
 ];
 
 if (!isDev) {
+    const MiniCssExtractPlugin = require('mini-css-extract-plugin');
     plugins.push(new MiniCssExtractPlugin({ filename: getPathName('css') }));
 
     if (process.env.NODE_ENV === 'stats') {
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
         plugins.push(new BundleAnalyzerPlugin());
     }
 }
 
 if (filesToBeCopy && filesToBeCopy.length) {
+    const CopyWebpackPlugin = require('copy-webpack-plugin');
     const patterns = getPatternsToCopy(filesToBeCopy, pathnames.src, pathnames.dist);
     plugins.push(new CopyWebpackPlugin(patterns));
 }
